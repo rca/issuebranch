54b83a2037d1d10af9eb5d1ee406294511a3b31a
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2018-01-18 14:27:25 -0500

    Make sure issue action type is returned for opened issues

--- src/webhook/tests/test_utils.py
+++ src/webhook/tests/test_utils.py
@@ -4,12 +4,19 @@ from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
 from webhook.utils import get_action_type
-from webhook.tests.utils import get_project_webhook_data
+from webhook.tests.utils import get_issue_opened_data, get_project_webhook_data
 
 class UtilsTestCase(TestCase):
-    def test_get_action_type(self):
+    def test_get_action_type_project(self):
         data = get_project_webhook_data()
 
         action_type = get_action_type(data)
 
         self.assertEquals('project', action_type)
+
+    def test_get_action_type_issue(self):
+        data = get_issue_opened_data()
+
+        action_type = get_action_type(data)
+
+        self.assertEquals('issue', action_type)
--- src/webhook/utils.py
+++ src/webhook/utils.py
@@ -1,6 +1,9 @@
 def get_action_type(webhook_data):
     action_type = None
 
+    if 'issue' in webhook_data:
+        return 'issue'
+
     if webhook_data.get('action') == 'moved':
         if webhook_data.get('project_card'):
             return 'project'
