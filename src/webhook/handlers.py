0a1792c0dc3253f7978c66048943ea82a696dd76
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2018-01-18 12:04:29 -0500

    Add ProjectHandler

---
+++ src/webhook/handlers.py
@@ -0,0 +1,101 @@
+import functools
+
+from issuebranch.backends.github import Backend, GithubSession
+
+ON_DECK_COLUMN_NAME = 'on deck'
+
+PRODUCT_BACKLOG_NAME = 'product backlog'
+SCRUM_BOARD_NAME = 'scrum board'
+
+OTHER_PROJECT = {
+    PRODUCT_BACKLOG_NAME: SCRUM_BOARD_NAME,
+    SCRUM_BOARD_NAME: PRODUCT_BACKLOG_NAME,
+}
+
+
+class ProjectHandler(object):
+    """
+    Handles project actions
+    """
+    def __init__(self, data):
+        self.data = data
+
+        self.session = GithubSession()
+
+    def add_to_on_deck(self, column_data):
+        # print(f'add_to_on_deck, column_data={column_data}')
+        issue_url = self.project_card_data['content_url']
+        project_data = self.get_request_data(column_data['project_url'])
+
+        other_project_data = self.get_other_project_data(project_data)
+        other_project_on_deck = self.get_project_on_deck(other_project_data)
+
+        card = self.find_card(other_project_on_deck, issue_url)
+        if card is None:
+            issue_id = int(issue_url.rsplit('/', 1)[-1])
+            issue_data = Backend(issue_id).issue
+
+            self.session.create_card(other_project_on_deck, issue_data)
+
+    def find_card(self, column_data, content_url):
+        # look for the card in the other board's on deck
+        for card in self.session.get_cards(column_data):
+            if card['content_url'] == content_url:
+                return card
+
+    def get_other_project_data(self, project_data):
+        other_project_name = OTHER_PROJECT.get(project_data['name'].lower())
+
+        return self.session.get_project(other_project_name)
+
+    def get_project_on_deck(self, project_data):
+        return self.session.get_column(project_data, ON_DECK_COLUMN_NAME)
+
+    @functools.lru_cache(maxsize=10)
+    def get_request_data(self, url):
+        return self.session.request('get', url).json()
+
+    @property
+    def project_card_data(self):
+        return self.data['project_card']
+
+    def run(self):
+        action = self.data['action']
+        if action != 'moved':
+            return
+
+        # check if something was added to the on deck column
+        column_url = self.project_card_data['column_url']
+        column_data = self.get_request_data(column_url)
+
+        if column_data['name'].lower() == ON_DECK_COLUMN_NAME:
+            self.add_to_on_deck()
+
+            return
+
+        # check if something was removed from the on deck column
+
+        # replace the url in the column url with the from column to see if the card was removed from on deck
+        from_column_url = column_url.replace(str(self.project_card_data['column_id']), str(self.data['changes']['column_id']['from']))
+        from_column_data = self.get_request_data(from_column_url)
+
+        if from_column_data['name'].lower() == ON_DECK_COLUMN_NAME:
+            self.remove_from_on_deck(from_column_data)
+
+            return
+
+    def remove_from_on_deck(self, column_data):
+        # print(f'add_to_on_deck, column_data={column_data}')
+        issue_url = self.project_card_data['content_url']
+        project_data = self.get_request_data(column_data['project_url'])
+
+        other_project_data = self.get_other_project_data(project_data)
+        other_project_on_deck = self.get_project_on_deck(other_project_data)
+
+        card_data = self.find_card(other_project_on_deck, issue_url)
+        if card_data is not None:
+            self.session.delete_card(card_data)
+
+handler_types = {
+    'project': ProjectHandler,
+}
---
+++ src/webhook/tests/test_handlers.py
@@ -0,0 +1,83 @@
+from unittest import mock
+
+from django.test import TestCase
+
+from webhook import handlers
+
+from webhook.tests.utils import get_project_webhook_data
+
+from_on_deck_project_data = {'action': 'moved', 'changes': {'column_id': {'from': 2001377}}, 'project_card': {'url': 'https://api.github.com/projects/columns/cards/6731956', 'column_url': 'https://api.github.com/projects/columns/2001376', 'column_id': 2001376, 'id': 6731956, 'note': None, 'creator': {'login': 'rca', 'id': 53537, 'avatar_url': 'https://avatars3.githubusercontent.com/u/53537?v=4', 'gravatar_id': '', 'url': 'https://api.github.com/users/rca', 'html_url': 'https://github.com/rca', 'followers_url': 'https://api.github.com/users/rca/followers', 'following_url': 'https://api.github.com/users/rca/following{/other_user}', 'gists_url': 'https://api.github.com/users/rca/gists{/gist_id}', 'starred_url': 'https://api.github.com/users/rca/starred{/owner}{/repo}', 'subscriptions_url': 'https://api.github.com/users/rca/subscriptions', 'organizations_url': 'https://api.github.com/users/rca/orgs', 'repos_url': 'https://api.github.com/users/rca/repos', 'events_url': 'https://api.github.com/users/rca/events{/privacy}', 'received_events_url': 'https://api.github.com/users/rca/received_events', 'type': 'User', 'site_admin': False}, 'created_at': '2018-01-16T17:49:17Z', 'updated_at': '2018-01-17T19:59:50Z', 'content_url': 'https://api.github.com/repos/openslate/openslate/issues/468', 'after_id': None}, 'organization': {'login': 'openslate', 'id': 1767240, 'url': 'https://api.github.com/orgs/openslate', 'repos_url': 'https://api.github.com/orgs/openslate/repos', 'events_url': 'https://api.github.com/orgs/openslate/events', 'hooks_url': 'https://api.github.com/orgs/openslate/hooks', 'issues_url': 'https://api.github.com/orgs/openslate/issues', 'members_url': 'https://api.github.com/orgs/openslate/members{/member}', 'public_members_url': 'https://api.github.com/orgs/openslate/public_members{/member}', 'avatar_url': 'https://avatars2.githubusercontent.com/u/1767240?v=4', 'description': ''}, 'sender': {'login': 'rca', 'id': 53537, 'avatar_url': 'https://avatars3.githubusercontent.com/u/53537?v=4', 'gravatar_id': '', 'url': 'https://api.github.com/users/rca', 'html_url': 'https://github.com/rca', 'followers_url': 'https://api.github.com/users/rca/followers', 'following_url': 'https://api.github.com/users/rca/following{/other_user}', 'gists_url': 'https://api.github.com/users/rca/gists{/gist_id}', 'starred_url': 'https://api.github.com/users/rca/starred{/owner}{/repo}', 'subscriptions_url': 'https://api.github.com/users/rca/subscriptions', 'organizations_url': 'https://api.github.com/users/rca/orgs', 'repos_url': 'https://api.github.com/users/rca/repos', 'events_url': 'https://api.github.com/users/rca/events{/privacy}', 'received_events_url': 'https://api.github.com/users/rca/received_events', 'type': 'User', 'site_admin': False}}
+
+to_on_deck_project_data = {'action': 'moved', 'changes': {'column_id': {'from': 2001376}}, 'project_card': {'url': 'https://api.github.com/projects/columns/cards/6731956', 'column_url': 'https://api.github.com/projects/columns/2001377', 'column_id': 2001377, 'id': 6731956, 'note': None, 'creator': {'login': 'rca', 'id': 53537, 'avatar_url': 'https://avatars3.githubusercontent.com/u/53537?v=4', 'gravatar_id': '', 'url': 'https://api.github.com/users/rca', 'html_url': 'https://github.com/rca', 'followers_url': 'https://api.github.com/users/rca/followers', 'following_url': 'https://api.github.com/users/rca/following{/other_user}', 'gists_url': 'https://api.github.com/users/rca/gists{/gist_id}', 'starred_url': 'https://api.github.com/users/rca/starred{/owner}{/repo}', 'subscriptions_url': 'https://api.github.com/users/rca/subscriptions', 'organizations_url': 'https://api.github.com/users/rca/orgs', 'repos_url': 'https://api.github.com/users/rca/repos', 'events_url': 'https://api.github.com/users/rca/events{/privacy}', 'received_events_url': 'https://api.github.com/users/rca/received_events', 'type': 'User', 'site_admin': False}, 'created_at': '2018-01-16T17:49:17Z', 'updated_at': '2018-01-17T19:26:28Z', 'content_url': 'https://api.github.com/repos/openslate/openslate/issues/468', 'after_id': None}, 'organization': {'login': 'openslate', 'id': 1767240, 'url': 'https://api.github.com/orgs/openslate', 'repos_url': 'https://api.github.com/orgs/openslate/repos', 'events_url': 'https://api.github.com/orgs/openslate/events', 'hooks_url': 'https://api.github.com/orgs/openslate/hooks', 'issues_url': 'https://api.github.com/orgs/openslate/issues', 'members_url': 'https://api.github.com/orgs/openslate/members{/member}', 'public_members_url': 'https://api.github.com/orgs/openslate/public_members{/member}', 'avatar_url': 'https://avatars2.githubusercontent.com/u/1767240?v=4', 'description': ''}, 'sender': {'login': 'rca', 'id': 53537, 'avatar_url': 'https://avatars3.githubusercontent.com/u/53537?v=4', 'gravatar_id': '', 'url': 'https://api.github.com/users/rca', 'html_url': 'https://github.com/rca', 'followers_url': 'https://api.github.com/users/rca/followers', 'following_url': 'https://api.github.com/users/rca/following{/other_user}', 'gists_url': 'https://api.github.com/users/rca/gists{/gist_id}', 'starred_url': 'https://api.github.com/users/rca/starred{/owner}{/repo}', 'subscriptions_url': 'https://api.github.com/users/rca/subscriptions', 'organizations_url': 'https://api.github.com/users/rca/orgs', 'repos_url': 'https://api.github.com/users/rca/repos', 'events_url': 'https://api.github.com/users/rca/events{/privacy}', 'received_events_url': 'https://api.github.com/users/rca/received_events', 'type': 'User', 'site_admin': False}}
+
+on_deck_column_data = {'url': 'https://api.github.com/projects/columns/2001377', 'project_url': 'https://api.github.com/projects/1206552', 'cards_url': 'https://api.github.com/projects/columns/2001377/cards', 'id': 2001377, 'name': 'On Deck', 'created_at': '2018-01-11T19:36:33Z', 'updated_at': '2018-01-17T19:26:28Z'}
+
+
+class ProjectHandlerTestCase(TestCase):
+    def test_move_grooming_card(self):
+        data = get_project_webhook_data()
+        project_handler = handlers.ProjectHandler(data)
+
+        project_handler.session = mock.Mock()
+        project_handler.session.request.return_value.json.return_value = {
+            'url': 'https://api.github.com/projects/columns/2001376',
+            'project_url': 'https://api.github.com/projects/1206552',
+            'cards_url': 'https://api.github.com/projects/columns/2001376/cards',
+            'id': 2001376,
+            'name': 'Grooming',
+            'created_at': '2018-01-11T19:36:33Z',
+            'updated_at': '2018-01-17T17:46:36Z'
+        }
+
+        project_handler.sync_on_deck = mock.Mock()
+
+        project_handler.run()
+
+        # a grooming card will not sync on deck
+        project_handler.sync_on_deck.assert_not_called()
+
+    def test_move_on_deck_card(self):
+        data = get_project_webhook_data()
+        project_handler = handlers.ProjectHandler(data)
+
+        project_handler.session = mock.Mock()
+        project_handler.session.request.return_value.json.return_value = {
+            'url': 'https://api.github.com/projects/columns/2001376',
+            'project_url': 'https://api.github.com/projects/1206552',
+            'cards_url': 'https://api.github.com/projects/columns/2001376/cards',
+            'id': 2001376,
+            'name': 'On Deck',
+            'created_at': '2018-01-11T19:36:33Z',
+            'updated_at': '2018-01-17T17:46:36Z'
+        }
+
+        project_handler.sync_on_deck = mock.Mock()
+
+        project_handler.run()
+
+        # an on deck card will sync!
+        project_handler.sync_on_deck.assert_called_with()
+
+    def test_detect_remove_from_on_deck(self):
+        project_handler = handlers.ProjectHandler(from_on_deck_project_data)
+        project_handler.remove_from_on_deck = mock.Mock()
+
+        project_handler.run()
+
+        project_handler.remove_from_on_deck.assert_called_with(mock.ANY)
+
+    def test_detect_add_to_on_deck(self):
+        project_handler = handlers.ProjectHandler(to_on_deck_project_data)
+        project_handler.add_to_on_deck = mock.Mock()
+
+        project_handler.run()
+
+        project_handler.add_to_on_deck.assert_called_with(mock.ANY)
+
+    def test_add_to_on_deck(self):
+        project_handler = handlers.ProjectHandler(to_on_deck_project_data)
+        project_handler.add_to_on_deck = mock.Mock()
+
+        project_handler.run()
+
+        project_handler.add_to_on_deck.assert_called_with(mock.ANY)
