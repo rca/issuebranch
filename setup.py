d26a4470fee1778041214206fec1aca3e6bab0b3
Jonathan Meier <jonathanwmeier@gmail.com>
2018-11-12 18:28:21 -0500

    Set version to 0.0.0
    We'll manage versions with tag-version now.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(
     name='issuebranch',
     url='https://github.com/rca/issuebranch',
-    version='0.0.3',
+    version='0.0.0',
     author='roberto aguilar',
     author_email='roberto.c.aguilar@gmail.com',
     package_dir={'': 'src'},
